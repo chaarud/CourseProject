f0002aae47c5f6d9a49914f466208b798a38cac0
Chaaru Dingankar <chaaru@crunchbase.com>
2021-12-07 14:14:27 -0800

    ensure unique recommendations

--- code/search_eval.py
+++ code/search_eval.py
@@ -1,5 +1,9 @@
+import sys
+
 import metapy
 import pytoml
+
+NUM_DOCS = 2000
 
 def input_yes(input):
     return input.lower().startswith("y") or input.lower().startswith("yes")
@@ -7,11 +11,12 @@ def input_yes(input):
 def input_no(input):
     return input.lower().startswith("n") or input.lower().startswith("no")
 
-def fetch_unique(idx, query, already_recommended):
-    page_size = 10
+def fetch_unique(idx, query, already_recommended, page_size):
+    if (page_size > NUM_DOCS):
+        print("No unique recommendations left!")
+        sys.exit(1)
 
     results = ranker.score(idx, query, page_size)
-
     for result in results:
         result_content = idx.metadata(result[0]).get("content")
         result_split = result_content.split(',', 1)
@@ -20,8 +25,7 @@ def fetch_unique(idx, query, already_recommended):
         if result_company_name not in already_recommended:
             return (result_company_name, result_company_desc)
 
-    # TODO fetch more!
-
+    fetch_unique(idx, query, already_recommended, page_size * 2)
 
 if __name__ == '__main__':
 
@@ -35,7 +39,7 @@ if __name__ == '__main__':
     query = metapy.index.Document()
 
     companies = {}
-    for i in range(2000):
+    for i in range(NUM_DOCS):
         record = idx.metadata(i).get("content")
         record_split = record.split(',', 1)
         company_name = record_split[0]
@@ -69,8 +73,8 @@ if __name__ == '__main__':
 
     already_recommended = preference_inputs
     while True:
-        print("Here's a recommendation for you, based on the companies you have already indicated preferring: ")
-        (result_company_name, result_company_desc) = fetch_unique(idx, query, already_recommended)
+        print("\nHere's a recommendation for you, based on the companies you have already indicated preferring: ")
+        (result_company_name, result_company_desc) = fetch_unique(idx, query, already_recommended, 10)
 
         already_recommended.append(result_company_name)
 
@@ -80,11 +84,12 @@ if __name__ == '__main__':
         feedback = input("Was your recommendation interesting? Please indicate Y/N: ")
         if input_yes(feedback):
             query.content(query.content() + " " + result_company_desc)
-        elif (not input_no(feedback)):
+        elif not input_no(feedback):
             print("Please input either Y or N. Considering recommendation uninteresting and proceeding.")
 
         proceed = input("Would you like to see more recommendations? Y/N: ")
         if input_no(proceed):
+            print("Goodbye!")
             break
         elif not input_yes(proceed):
             print("Please input either Y or N. Completing recommendations.")
